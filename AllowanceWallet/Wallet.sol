1d145e16c565b9ba4f6f8a553e9bc7df894eb6ef
Kaustubh <kaustubhd287@gmail.com>
2022-03-01 18:40:29 +0000

    Emits events for deposits and withdraw processes

--- AllowanceWallet/Wallet.sol
+++ AllowanceWallet/Wallet.sol
@@ -4,15 +4,20 @@ pragma solidity ^0.8.7;
 import "./Allowance.sol";
 
 contract Wallet is Allowance {
+    event MoneyWithdrawn(address _sent_to, uint _amount);
+    event MoneyReceived(address _from, uint _amount);
+
     function withdraw(address payable _to, uint _amount) public ownerOrValidAmount(_amount) {
         require(_amount <= address(this).balance, "Funds not enough in the smart contract");
         if (!_isOwner()) {
             _decreaseBalance(_to, _amount);
         }
+        emit MoneyWithdrawn(_to, _amount);
         _to.transfer(_amount);
     }
 
     receive() external payable {
+        emit MoneyReceived(_msgSender(), msg.value);
     }
 
     fallback() external payable {
