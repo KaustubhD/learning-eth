5df570ec70aa414ef27048cbb2c85b32cce944f2
Kaustubh <22456687+KaustubhD@users.noreply.github.com>
2022-03-01 22:56:24 +0530

    Adds set allowance function
    Adds fallback and receive functions

--- AllowanceWallet/Wallet.sol
+++ AllowanceWallet/Wallet.sol
@@ -4,7 +4,28 @@ pragma solidity ^0.8.7;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract Wallet is Ownable {
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+
+    mapping(address => uint) public allowance;
+
+    // Set how much a user is allowed to withdraw
+    function setAllowanceAmount(address _to, uint _amount) public onlyOwner {
+        allowance[_to] = _amount;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrValidAmount(_amount) {
         _to.transfer(_amount);
+    }
+
+    modifier ownerOrValidAmount(uint _amount) {
+        require(owner() == _msgSender() || allowance[msg.sender] >= _amount, "Invalid operation");
+        _;
+    }
+
+    receive() external payable {
+
+    }
+    
+    fallback() external payable {
+
     }
 }
