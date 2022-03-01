8c7731b8a99dfc36a63df798177d7b54b144e985
Kaustubh <22456687+KaustubhD@users.noreply.github.com>
2022-03-01 22:15:35 +0530

    Adds zepplin contract to handle ownership

--- AllowanceWallet/Wallet.sol
+++ AllowanceWallet/Wallet.sol
@@ -1,18 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Wallet {
-    address public owner;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Only owner is allowed to perform this operation");
-        _;
-    }
-
+contract Wallet is Ownable {
     function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
     }
