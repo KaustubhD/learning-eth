e95396eef01462a80a51631c6e73848987bf2a08
KaustubhD <kaustubhd287@gmail.com>
2022-03-12 23:42:52 +0530

    Adds more unit tests

--- Projects/03-Tokenisation/test/Uno.test.js
+++ Projects/03-Tokenisation/test/Uno.test.js
@@ -11,9 +11,34 @@ chai
 
 
 contract('Token test', async (accounts) => {
-  it('Should contain all token in creator account', async () => {
-    const instance = await TokenContract.deployed()
-    const totalSupply = await instance.totalSupply()
-    expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply)
+  const totalSupply = new BN(100000)
+  const [deployerAccount, recipientAccount, anotherAccount] = accounts
+  let instance
+
+  beforeEach(async () => {
+    instance = await TokenContract.new(totalSupply)
+  })
+
+
+  it('Should contain all tokens in creator account', async () => {  
+    expect(await instance.balanceOf(deployerAccount)).to.be.a.bignumber.equal(totalSupply)
+  })
+
+  it('Is possible to transfer tokens between accounts', async () => {
+    const numTokens = 1
+    const senderBalance = await instance.balanceOf(deployerAccount)
+    const recipientBalance = await instance.balanceOf(recipientAccount)
+    
+    await instance.transfer(recipientAccount, numTokens)
+    const newSenderBalance = await instance.balanceOf(deployerAccount)
+    const newRecipientBalance = await instance.balanceOf(recipientAccount)
+
+    expect(newSenderBalance).to.be.a.bignumber.equal(senderBalance.sub(new BN(numTokens)))
+    expect(newRecipientBalance).to.be.a.bignumber.equal(recipientBalance.add(new BN(numTokens)))
+  })
+
+  it('Is not possible to send more token than totalSupply', async () => {
+    await expect(instance.transfer(recipientAccount, totalSupply.add(new BN(1)))).to.eventually.be.rejected
+    await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply)
   })
 })
