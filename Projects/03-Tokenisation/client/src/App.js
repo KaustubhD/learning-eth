ec9752f101076a42a5f9ab05aa2832991992a142
KaustubhD <kaustubhd287@gmail.com>
2022-03-13 22:46:46 +0530

    Adds buy token form Updates token balance by listening to event Updates html structure

--- Projects/03-Tokenisation/client/src/App.js
+++ Projects/03-Tokenisation/client/src/App.js
@@ -7,7 +7,12 @@ import KycContract from './contracts/Kyc.json'
 import './App.css'
 
 class App extends Component {
-  state = { isLoaded: false, kycAddress: '' }
+  state = {
+    isLoaded: false,
+    kycAddress: '',
+    tokenBalance: 0,
+    numTokensToBuy: 0
+  }
 
   componentDidMount = async () => {
     try {
@@ -34,8 +39,14 @@ class App extends Component {
         kycAddress
       )
 
-      this.setState({ ...this.state, isLoaded: true, tokenSaleAddress: tokenSaleContractAddress, tokenAddress: tokenContractAddress})
-      console.log(this.state)
+      this.listenToTokenTransferEvent()
+
+      this.setState({
+        ...this.state,
+        isLoaded: true,
+        tokenSaleAddress: tokenSaleContractAddress,
+        tokenAddress: tokenContractAddress,
+      }, this.updateTokenBalance)
     } catch (error) {
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
@@ -55,9 +66,28 @@ class App extends Component {
     })
   }
 
+  listenToTokenTransferEvent = () => {
+    this.tokenInstance.events.Transfer({to: this.accounts[0]}).on('data', this.updateTokenBalance)
+  }
+
+  updateTokenBalance = async () => {
+    let tokenBalance = await this.tokenInstance.methods.balanceOf(this.accounts[0]).call()
+    this.setState({
+      ...this.state,
+      tokenBalance
+    })
+  }
+
   handleKycWhitelist = async () => {
     await this.kycInstance.methods.setKycCompleted(this.state.kycAddress).send({ from: this.accounts[0] })
     console.log('KYC completed for ' + this.state.kycAddress)
+  }
+
+  handleBuyToken = async () => {
+    if (this.state.numTokensToBuy <= 0)
+      alert('Stop playing and enter a valid value')
+    
+    this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({ from: this.accounts[0], value: this.state.numTokensToBuy.toString() })
   }
 
   render() {
@@ -67,21 +97,39 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>Uno Token crowdsale</h1>
-        <div>
-          <span>Add address to allow purchase</span>
-          <input type="text" name="kycAddress" value={this.state.kycAddress} placeholder="0x123" onChange={this.handleInputChange}/>
-          <button onClick={this.handleKycWhitelist}>Add to whitelist</button>
+        <p>
+          1 UNO = 1 wei (10<sup>18</sup> eth)
+        </p>
+        <div id="container">
+          <div id="id-div">
+            <h3>KYC</h3>
+            <p>Add address to allow purchase</p>
+            <input type="text" name="kycAddress" value={this.state.kycAddress} placeholder="0x123" onChange={this.handleInputChange}/>
+            <button onClick={this.handleKycWhitelist}>Add to whitelist</button>
+          </div>
+          <div id="buy-div">  
+            <h3>How to buy</h3>
+            <p>
+              To buy Uno, please send ether to <pre>{this.state.tokenSaleAddress}</pre>
+            </p>
+            <p>
+              Or enter number of tokens in the box<br />
+              <input type="number" name="numTokensToBuy" value={this.state.numTokensToBuy} onChange={this.handleInputChange}/>
+              <button onClick={this.handleBuyToken}>Buy tokens</button>
+            </p>
+          </div>
+          <div id="add-token-div">
+            <h3>How to view UNO in metamask ?</h3>
+            <p>
+              To see your UNO tokens in metamask add the token using address: <pre>{this.state.tokenAddress}</pre>
+            </p>
+          </div>
+        <div id="balance-div">
+          <h3>Balance</h3>
+          <p>
+            Your balance: {this.state.tokenBalance} UNO
+          </p>
         </div>
-        <div>
-          <p>
-            To buy Uno, please send ether to {this.state.tokenSaleAddress}
-          </p>
-          <p>
-            1 UNO = 1 wei (10<sup>18</sup> eth)
-          </p>
-          <p>
-            To see your UNO tokens in metamask add the token using address: {this.state.tokenAddress}
-          </p>
         </div>
       </div>
     )
